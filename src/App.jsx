765a83afb569e24b92136f1f0134babcea3db4a9
maelchaine <chaine.mael@gmail.com>
2024-09-25 18:18:23 +0200

    Use dynamic list for characters

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import characters from './data/characters.json'
 
 function App() {
   return (
     <>
       <h1>Marvel Characters</h1>
       <ul id="characters">
-        <li>
-          Beast
-        </li>
-        <li>
-          Captain America
-        </li>
-        <li>
-          Deadpool
-        </li>
+        {characters.map((character)=>(
+          <li key={character.id}>
+            {character.name}
+          </li>
+        ))}
       </ul>
     </>
   )
