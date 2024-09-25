d9c426ec2f09caacfed48711241a9872a9dfecad
maelchaine <chaine.mael@gmail.com>
2024-09-25 18:00:16 +0200

    Use static list for characters

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Marvel Characters</h1>
+      <ul id="characters">
+        <li>
+          Beast
+        </li>
+        <li>
+          Captain America
+        </li>
+        <li>
+          Deadpool
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default App
+
