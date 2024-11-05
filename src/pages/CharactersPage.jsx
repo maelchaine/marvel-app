66b9ffb7ef18fa4bbc2ab7b9cb121f4e446805c4
maelchaine <chaine.mael@gmail.com>
2024-11-05 18:12:07 +0100

    update code

--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -2,21 +2,24 @@
 // react_router nous permet de faire mieux en utilisant des hooks pour récupérer les données avant d'afficher le composant et ainsi de séparer récupération des données et affichage.
 // Adapter le code pour utiliser react_router et les hooks comme dans l'exemple du guide, grâce aux concepts de loader et useLoaderData . On appelera directement la fonction getCharacters dans le loader (pas de fonction fetch ).
 
-import { useLoaderData } from "react-router-dom";
+import React from 'react';
+import {CharactersList} from '../components/CharactersList';
+import {NumberOfCharacters} from '../components/NumberOfCharacters';
+import { useLoaderData } from "react-router";
+
 
 export default function CharactersPage() {
     const characters = useLoaderData(); // Assurez-vous que cette fonction soit importée correctement
 
+      // Modifier le titre de la page
+    document.title = "Marvel App"; // Changer le titre de la page
+
     return (
         <div>
             <h1>Liste des personnages</h1>
-            <ul>
-                {characters.map(character => (
-                    <li key={character.id}>
-                        <a href={`/character/${character.id}`}>{character.name}</a>
-                    </li>
-                ))}
-            </ul>
+            <CharactersList characters={characters} />
+
+            <NumberOfCharacters characters={characters} />
         </div>
     );
 }
